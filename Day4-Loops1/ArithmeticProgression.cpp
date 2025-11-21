4892146ee532eded2e9476b83100c111c214d75c
VyomGarg04 <vyomgarg497@gmail.com>
2025-11-21 21:59:58 +0530

    addition of programs

---
+++ Day4-Loops1/ArithmeticProgression.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,d,n;
+    cout<<"Enter the first number of the series :";
+    cin>>a;
+    cout<<"Enter the difference between the terms :";
+    cin>>d;
+    cout<<"Enter no of terms :";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<a<<endl;
+        a+=d;
+    }
+    // for(int i=5;i<=3*n+2;i+3)
+}
