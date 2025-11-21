aba2302d6c18692dbb8484aeb65fa9ce35d639a3
VyomGarg04 <vyomgarg497@gmail.com>
2025-11-21 21:18:49 +0530

    changes

--- DAY_3-Conditional/3DigitNoOrNot.cpp
+++ DAY_3-Conditional/3DigitNoOrNot.cpp
@@ -5,8 +5,8 @@ int main(){
     cout<<"Enter a number: ";
     cin>>a;
     if(a>99 && a<1000)
-        cout<<"The Entered number is a three digit number";
+        cout<<"The Entered number is a three digit Number ";
     else
-        cout<<"The entered number is not a three digit Number";
+        cout<<"The entered number is not a three digit Number ";
 
 }
