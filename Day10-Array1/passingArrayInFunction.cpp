dafa0673a755212cbb5698b7c1713ea686036db3
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-15 21:39:32 +0530

    changes in program

--- Day10-Array1/passingArrayInFunction.cpp
+++ Day10-Array1/passingArrayInFunction.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void display(int arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl; 
-}
-
 void sum(int arr[], int size) {
     int sum = 0;
     for (int i = 0; i < size; i++) {
         sum += arr[i];
     }
-    cout<<"Sum = "<<sum; // return the sum
+    cout<<"Sum = "<<sum; 
 }
 
+void disp(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl; 
+}
 int main() {
     int arr[] = {2, 4, 6, 8, 10};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    display(arr, size);
+    disp(arr, size);
 
     sum(arr, size);
     
