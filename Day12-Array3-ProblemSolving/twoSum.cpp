62c129235501b6ab23f48d5ae8cf4d886d3cddbb
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-17 21:42:27 +0530

    add vector operations examples including input, sorting, and size management

---
+++ Day12-Array3-ProblemSolving/twoSum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    cout<<"Enter the size of array"
+    vector<int> v;
+
+    
+    
+    int x;
+    cout<<"Enter your Target";
+    cin>>x;
+}
