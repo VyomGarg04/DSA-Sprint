f6c4ba4f3b5fa55cc0736fcfeefe12e81c98a0bf
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-17 21:54:14 +0530

    two sum program

--- Day12-Array3-ProblemSolving/twoSum.cpp
+++ Day12-Array3-ProblemSolving/twoSum.cpp
@@ -4,12 +4,24 @@
 using namespace std;
 
 int main(){
-    cout<<"Enter the size of array"
+    cout<<"Enter the size of array : ";
+    int size;
+    cin>>size;
     vector<int> v;
+    for(int i=0;i<size;i++){
+        int e;
+        cin>>e;
+        v.push_back(e);
+    }
 
-    
-    
-    int x;
-    cout<<"Enter your Target";
-    cin>>x;
+    int target;
+    cout<<"Enter your Target : ";
+    cin>>target;
+
+    for(int i=0;i<v.size()-1;i++){
+        for(int j=i+1;j<v.size();j++){
+            if(v[i]+v[j]==target)
+                cout<<i<<" "<<j;
+        }
+    }
 }
