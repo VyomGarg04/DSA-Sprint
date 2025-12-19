c3e5b86650150585b343918163d96b88503b6012
VyomGarg04 <vyomgarg497@gmail.com>
2025-12-19 21:45:06 +0530

    rotateArray leetcode problem 189

---
+++ Day13-Array4-ProblemSolving/rotateArray.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+//two pointer(iterator) approach
+void display(vector<int> &v){
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+}
+vector<int> rotate(int i,int j,vector<int> &v){
+    while(i<=j){
+        int temp = v[i];
+        v[i]=v[j];
+        v[j]=temp;
+        i++;
+        j--;
+    }
+    return v;
+}
+int main(){
+    cout<<"Enter the size of array : ";
+    int size;
+    cin>>size;
+    vector<int> v;
+    for(int i=0;i<size;i++){
+        int e;
+        cin>>e;
+        v.push_back(e);
+    }
+
+    cout<<"Input array : "<<endl;
+    display(v);
+
+
+    cout<<"\nNo of steps you want to rotate your array : "<<endl;
+    int k;
+    cin>>k;
+    
+    int n = v.size();
+    rotate(n-k,n-1,v);
+    rotate(0,n-k-1,v);
+    rotate(0,n-1,v);
+
+    cout<<"After rotating : "<<endl;
+    display(v);
+    
+    
+    
+    
+}
