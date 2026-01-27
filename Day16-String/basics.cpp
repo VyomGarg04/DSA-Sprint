32cee5fe997dbfd7db2486a3e3ea80ac9cdba49d
VyomGarg04 <vyomgarg497@gmail.com>
2026-01-27 19:03:38 +0530

    added new programs

---
+++ Day16-String/basics.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char chr = 'a';
+
+    char ch[]={'a','b','c','d','e','f'};
+}
---
+++ Day16-String/input.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // char ch[2] = {'a','b'};
+    //it do not need a for loop to take input
+    // cout<<(int)ch[3];
+
+    // char ch[]={'a','b','c','d','e','f'};
+    // for(int i=0;ch[i]!='\0';i++){
+    //     cout<<ch[i]<<endl;
+    // }
+
+
+    char ch[10];
+    cin.getline()
+
+
+}
