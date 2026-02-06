1388e3561560b2e5228ee916835713c5fd8cd28f
VyomGarg04 <vyomgarg497@gmail.com>
2026-02-06 21:20:36 +0530

    added programs

--- Day19-BinarySearch/firstOccurance.cpp
+++ Day19-BinarySearch/firstOccurance.cpp
@@ -1,28 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int arr[]={0,1,1,2,2,2,2,2,3,3,4,7,10};
-    int n =13;
-    int x=2;
-    int lo=0;
-    int hi=n-1;
-    bool flag=false; //element is not persent in array
-    while(lo<=hi){
-        int mid = lo +(hi-lo)/2;
-        
-        if(arr[mid]==x){
-            if(mid==0){
-                flag=true;
-                cout<<mid;
-            }
-            else if(arr[mid-1]!=x){
-                flag = true;
-                cout<<mid;
-                break;
-            }
-            else 
+
+int main() {
+    int arr[] = {0,1,1,2,2,2,2,2,3,3,4,7,10};
+    int n = 13;
+    int x = 2;
+    int lo = 0;
+    int hi = n - 1;
+    int firstIndex = -1;   // stores the first occurrence
+
+    while (lo <= hi) {
+        int mid = lo+(hi-lo) / 2;
+
+        if (arr[mid] == x) {
+            firstIndex = mid;
+            hi = mid - 1;
         }
-        else if(arr[mid]<x) lo =mid +1;
-        else hi =mid-1;
+        else if (arr[mid] < x) {
+            lo = mid + 1;
+        }
+        else {
+            hi = mid - 1;
+        }
     }
+
+    if (firstIndex != -1)
+        cout << "First occurrence of " << x << " is at index " << firstIndex << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
 }
